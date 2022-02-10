898d0471e16bff1e48f9ee36a978bfbbdf554879
ColinTate24 <97544852+ColinTate24@users.noreply.github.com>
2022-02-10 11:21:22 -0600

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,66 @@
+$(document).ready(function () {
+  console.log("doc is ready");
+ 
+  let revName; 
+
+
+  $
+    ("#btnGetFacts").click(function (){
+
+      // handle it if input is blank
+
+      let userName = $("#userName").val();
+      let coolFacts = generateCoolNameFacts(userName); 
+
+      console.log('you clicked the button');
+      console.log('from ln 12', generateCoolNameFacts(userName));
+      
+      $("#coolNameFactsOutput").html(coolFacts);
+  
+
+
+    });
+
+
+});
+
+function generateCoolNameFacts(userName) {
+  let coolFacts = ''; 
+  
+  coolFacts = nameLength(userName);
+  coolFacts += " <br> " + nameStart(userName);
+  coolFacts += "<h3> " + nameEnd(userName) + "</h3>";
+  coolFacts += "<h3> " + spiritAnimal(userName);
+
+  console.log("initialized the'm coolFacts variable: " + coolFacts); 
+
+  return coolFacts;
+}
+
+function nameLength(name) {
+  return "Your name is " + name.length + " letters long!"; 
+}
+
+function nameStart(name) {
+  return "the first letter of your name is " + name[0]
+}
+
+function nameEnd(name) {
+  
+  let lastIndex = name.length - 1
+
+  return "the last letter of your name is " + name[lastIndex]
+}
+
+function spiritAnimal(name) {
+
+  if (name.length <= 5) {
+    return "your spirit animal is a chinchilla"
+  }
+  if (name.length > 6) {
+    return "your spirit animal is a tiger"
+  }
+  if (name == "cumbie") {
+    return "your spirit animal is not a pokemon"
+  }
+}
